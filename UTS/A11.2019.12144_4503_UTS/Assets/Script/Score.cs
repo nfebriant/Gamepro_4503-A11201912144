86c259613a01d9e637333ad9bb0d8a0123952d8a
nfebriant <73210718+nfebriant@users.noreply.github.com>
2021-11-15 15:04:28 +0700

    UTS GAMEPRO

---
+++ UTS/A11.2019.12144_4503_UTS/Assets/Script/Score.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    public Text MyscoreText;
+    private int ScoreNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreNum = 0;
+        MyscoreText.text = "Score : " + ScoreNum;
+
+    }
+
+    // Update is called once per frame
+    
+    private void OnTriggerEnter2D(Collider2D Coin)
+    {
+        if (Coin.tag == "Coin")
+        {
+            ScoreNum += 15;
+            Destroy(Coin.gameObject);
+            MyscoreText.text = "Score : " + ScoreNum;
+        }
+    }
+    
+}
